a573fa7630a33353f136e74793bdb5762740e49c
Francois Vogel <francois.g.vogel@gmail.com>
2021-07-06 15:42:03 +0200

    5th commit

--- create/creator.cpp
+++ create/creator.cpp
@@ -383,9 +383,8 @@ void parse(string toParsePath) {
 
 int main() {
     announce("Please enter the parent directory (with a '/' at the end) of your creator.cpp file (in normal use cases you should just enter \"./\")");
-    // ifstream fin("./tasks.in");
-    #define fin cin
-    // fin >> root;
+    ifstream fin("./tasks.in");
+    // #define fin cin
     fin >> root;
     while (true) {
         announce("Please enter the location of the file you'd like to parse: (e.g. ./toParse.txt) (other special commands feature '0': stop execution)");
